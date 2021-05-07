c5b7152a77ff583a40c7c0ec92e874737f4e95bd
Lubia <gonzalesmilagro20@gmail.com>
2021-05-06 19:38:43 -0600

    PRACTICA 2- CINEMA

---
+++ tbl_cartelera.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-05-2021 a las 03:33:55
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_cinema`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_cartelera`
+--
+
+CREATE TABLE `tbl_cartelera` (
+  `ID_CARTELERA` int(11) NOT NULL,
+  `PELICULA` varchar(200) NOT NULL,
+  `HORA` int(100) NOT NULL,
+  `FECHA` int(100) NOT NULL,
+  `IDIOMA` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tbl_cartelera`
+--
+
+INSERT INTO `tbl_cartelera` (`ID_CARTELERA`, `PELICULA`, `HORA`, `FECHA`, `IDIOMA`) VALUES
+(2, 'Rapunzel', 8, 10, 'Espanol'),
+(3, 'Dos metros de ti', 9, 13, 'Espanol'),
+(4, 'bajo la misma estrella', 8, 27, 'Ingles');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tbl_cartelera`
+--
+ALTER TABLE `tbl_cartelera`
+  ADD PRIMARY KEY (`ID_CARTELERA`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_cartelera`
+--
+ALTER TABLE `tbl_cartelera`
+  MODIFY `ID_CARTELERA` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
